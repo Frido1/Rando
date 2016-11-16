f6bc7dae2f5a9cb3fe1bddcfcb15b011653e2b6e
Frido1 <fjmartinez3@gmail.com>
2016-11-16 17:29:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/frido/rando/Home.java
@@ -0,0 +1,132 @@
+package com.example.frido.rando;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Home extends Activity {
+    @BindView(R.id.home_WelcomeRando)
+    TextView welcomeTitle;
+    @BindView(R.id.home_Button_Start)
+    Button homeButtonStart;
+    @BindView(R.id.home_ViewSentPics)
+    Button viewSentPics;
+    @BindView(R.id.home_PersonalHistory)
+    Button personalHistory;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String TAG = "Home";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Intent intent = new Intent(getApplicationContext(), SignIn.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void menuLogOut(MenuItem mi) {
+        Log.d("test", "menu fire");
+
+
+        // Google sign out
+        final GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API).build();
+        mGoogleApiClient.connect();
+        mGoogleApiClient.registerConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(@Nullable Bundle bundle) {
+
+
+                if (mGoogleApiClient.isConnected()) {
+                    Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(@NonNull Status status) {
+                            if (status.isSuccess()) {
+                                Log.d(TAG, "User Logged out");
+                                mAuth.signOut();
+                                finish();
+                            }
+                        }
+                    });
+                }
+
+            }
+
+
+            @Override
+            public void onConnectionSuspended(int i) {
+                Log.d(TAG, "connedctionSusspended");
+            }
+        });
+        mAuth.signOut();
+    }
+
+    public void startMainPictureDisplayActivity(View view){
+        Intent intent = new Intent(getApplicationContext(),MainPictureDisplay.class);
+        startActivity(intent);
+    }
+}
+
+
