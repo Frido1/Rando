5d181c0a04dac2232ac61d3198afcc8d5b98d238
Frido1 <fjmartinez3@gmail.com>
2016-11-19 11:01:37 -0500

    tried captureing the touch event from the slider didn't work

--- app/src/main/java/com/example/frido/rando/Home.java
+++ app/src/main/java/com/example/frido/rando/Home.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.nfc.Tag;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
@@ -33,6 +36,8 @@ public class Home extends Activity {
     Button viewSentPics;
     @BindView(R.id.home_PersonalHistory)
     Button personalHistory;
+    @BindView(R.id.activity_home)
+    RelativeLayout testLayout;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String TAG = "Home";
@@ -64,12 +69,14 @@ public class Home extends Activity {
             }
         };
 
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+
     }
 
     @Override
--- app/src/main/java/com/example/frido/rando/ImageViewFullscreen.java
+++ app/src/main/java/com/example/frido/rando/ImageViewFullscreen.java
@@ -1,35 +1,26 @@
 package com.example.frido.rando;
 
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
-import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import com.bumptech.glide.load.resource.bitmap.FitCenter;
 
-import java.util.ArrayList;
-import java.util.Random;
-
-import butterknife.BindView;
 import it.sephiroth.android.library.imagezoom.ImageViewTouch;
-import it.sephiroth.android.library.imagezoom.ImageViewTouchBase;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class MainPictureDisplay extends Activity {
+public class ImageViewFullscreen extends AppCompatActivity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -48,7 +39,7 @@ public class MainPictureDisplay extends Activity {
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    private SwipeFlingAdapterView mContentView;
+    private ImageViewTouch mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
@@ -58,7 +49,6 @@ public class MainPictureDisplay extends Activity {
             // Note that some of these constants are new as of API 16 (Jelly Bean)
             // and API 19 (KitKat). It is safe to use them, as they are inlined
             // at compile-time and do nothing on earlier devices.
-
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -72,7 +62,7 @@ public class MainPictureDisplay extends Activity {
         @Override
         public void run() {
             // Delayed display of UI elements
-            ActionBar actionBar = getActionBar();
+            ActionBar actionBar = getSupportActionBar();
             if (actionBar != null) {
                 actionBar.show();
             }
@@ -100,115 +90,43 @@ public class MainPictureDisplay extends Activity {
             return false;
         }
     };
-    private  String imageURLFatPita = "http://fatpita.net/images/image%20";
-    private final int Total_FATPITA_Images = 20240;
-    private ArrayList<String> imagesToLoad = new ArrayList<String>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Context context = getApplicationContext();
 
-        setContentView(R.layout.activity_main_picture_display);
-        ActionBar actionBar = getActionBar();
+        setContentView(R.layout.activity_image_view_fullscreen);
+        ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = (SwipeFlingAdapterView) findViewById(R.id.flingContainerFrame);
+        mControlsView = findViewById(R.id.fullscreenImage_center_horizontal);
+        mContentView = (ImageViewTouch) findViewById(R.id.fullscreenImage_content);
+
+        Glide.with(context)
+                .load(getIntent().getStringExtra("imageToView"))
+                .bitmapTransform(new CroppingTransformation(context), new FitCenter(context))
+                .into(mContentView);
+        mContentView.setScaleType(ImageViewTouch.ScaleType.CENTER);
 
 
 
         // Set up the user interaction to manually show or hide the system UI.
-        //ToDo
-
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle();
+            }
+        });
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-
-
-        //load image into main content view
-        imagesToLoad = getImageURLS();
-        final CustomImageViewAdapater adapater = new CustomImageViewAdapater(getApplicationContext(),imagesToLoad);
-        mContentView.setAdapter(adapater);
-        mContentView.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
-                                            @Override
-                                            public void removeFirstObjectInAdapter() {
-                                                // this is the simplest way to delete an object from the Adapter (/AdapterView)
-                                                Log.d("LIST", "removed object!");
-                                                imagesToLoad.remove(0);
-                                                adapater.notifyDataSetChanged();
-                                            }
-
-                                            @Override
-                                            public void onLeftCardExit(Object dataObject) {
-                                                //Do something on the left!
-                                                //You also have access to the original object.
-                                                //If you want to use it just cast it (String) dataObject
-                                                Toast.makeText(MainPictureDisplay.this, "Left!", Toast.LENGTH_SHORT).show();
-                                            }
-
-                                            @Override
-                                            public void onRightCardExit(Object dataObject) {
-                                                Toast.makeText(MainPictureDisplay.this, "Right!", Toast.LENGTH_SHORT).show();
-                                            }
-
-            @Override
-            public void onAdapterAboutToEmpty(int i) {
-                imagesToLoad .add(getOneMoreImage());
-               adapater.notifyDataSetChanged();
-                Log.d("LIST", "notified");
-                i++;
-
-            }
-
-            @Override
-            public void onScroll(float v) {
-
-            }
-
-        });
-        mContentView.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClicked(int i, Object o) {
-                View view = mContentView.getSelectedView();
-                Intent intent = new Intent(getApplicationContext(),ImageViewFullscreen.class);
-                intent.putExtra("imageToView", ""+o);
-                startActivity(intent);
-
-
-            }
-        });
-
-
-
-
     }
-
-    private String getOneMoreImage() {
-        Random rand = new Random();
-        int fatPitaImage =  rand.nextInt(Total_FATPITA_Images)+1;
-        imageURLFatPita = imageURLFatPita+"("+fatPitaImage+").jpg";
-        return imageURLFatPita ;
-    }
-
-    private ArrayList<String> getImageURLS() {
-        Random rand = new Random();
-        ArrayList<String> temp = new ArrayList<String>();
-        String tempURL;
-        for (int i = 0; i < 10 ; i++) {
-            int fatPitaImage =  rand.nextInt(Total_FATPITA_Images)+1;
-            tempURL = imageURLFatPita+"("+fatPitaImage+").jpg";
-            temp.add(tempURL);
-        }
-        return temp;
-    }
-
-
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -241,7 +159,7 @@ public class MainPictureDisplay extends Activity {
 
     private void hide() {
         // Hide UI first
-        ActionBar actionBar = getActionBar();
+        ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
         }
